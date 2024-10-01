9f58b62fce29614d4159db775a7cfc64e2cb02de
Rainbow Market <helpdisk.rainbowmarket@gmail.com>
2024-10-01 20:38:29 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import './style/index.css';
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-// import RajaRani from './rajarani';
-// import OvalTable from './test';
-// import HomeScreen from './homepage';
 import CreateGroupOrJoinGroup from './screen/creategrouporjoingroup';
 import CreateGroup from './screen/creategroup';
 import JoinGroup from './screen/joingroup';
@@ -22,9 +19,6 @@ root.render(
         <Route path="/CreateGroup" element={<CreateGroup />} />
         <Route path="/joingroup" element={<JoinGroup />} />
         <Route path="/Group/:id/:username" element={<StartGame />} />
-        {/* <Route path="/test" element={<OvalTable />} />
-        <Route path="/start" element={<HomeScreen />} />
-        <Route path="/start/:username" element={<RajaRani />} /> */}
       </Routes>
     </Router>
   </React.StrictMode>
