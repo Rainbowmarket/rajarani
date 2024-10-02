b381236b12c6bb2733c45bc44dfe35ed6128f798
Rainbow Market <helpdisk.rainbowmarket@gmail.com>
2024-10-02 08:55:36 +0530

    Update startgame.js

--- src/screen/startgame.js
+++ src/screen/startgame.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { getDatabase, ref, get, update } from 'firebase/database';
-import app from '../firebase/db';
+import app from '../firebase/connection';
 import '../style/StartGame.css';
 
 const db = getDatabase(app);
